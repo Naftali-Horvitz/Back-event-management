bc51719f6f259443afe66d60c87058e7de8acc42
Naftali Horviz <n0548498093@gmail.com>
2026-01-12 11:28:20 +0200

    fix app.js

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const path = require("path"); // הוסף את מודול path להגשת קבצים סטטיים
-const userRouter = require("./routers/userRouter");
-const guestRouter = require('./routers/guestRouter');
-const eventRouter = require('./routers/eventRouter');
-const Guest = require('./models/guestModel');
-const app = express();
-const port = process.env.PORT || 5000;
 
+const userRouter = require("./routers/userRouter");
+const guestRouter = require("./routers/guestRouter");
+const eventRouter = require("./routers/eventRouter");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-mongoose.connect(uri)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection failed:", err);
+  });
 
-// הגדרת הקבצים הסטטיים של React
-app.use(express.static(path.join(__dirname, 'build')));
-
-// ניתובים קיימים
-app.use("/user/", userRouter);
-app.use('/guest/', guestRouter);
-app.use('/events/', eventRouter);
-
-// Catch-All Route לכל הבקשות האחרות
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// routes
+app.use("/user", userRouter);
+app.use("/guest", guestRouter);
+app.use("/events", eventRouter);
